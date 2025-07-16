aa010f1d0533bd9fba94a45b7707b8b7129f66e9
Aru Gyani <gyaniaru@gmail.com>
2025-07-15 23:11:30 -0400

    content: adds write-up for current site state

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,8 +1,6 @@
 import { MDXComponents } from "mdx/types";
 import Link from "next/link";
-import { FC, HTMLProps } from "react";
-
-import cn from "clsx";
+import { FC } from "react";
 
 /** TODO
  *    1) Finish styling all headings
@@ -10,26 +8,38 @@ import cn from "clsx";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const components: Record<string, FC<any>> = {
-  h1: (props: HTMLProps<HTMLHeadingElement>) => (
-    <h1
-      className="font-bold mb-7 text-balance text-lapis-600 dark:text-light"
+  h1: (props) => (
+    <h1 className="font-semibold mb-7 text-lapis-600 text-balance" {...props} />
+  ),
+  h2: (props) => (
+    <h2
+      className="font-semibold mt-14 mb-7 text-lapis-600 text-balance"
       {...props}
     />
   ),
-  h2: (props: HTMLProps<HTMLHeadingElement>) => <h2 {...props} />,
-  h3: (props: HTMLProps<HTMLHeadingElement>) => <h3 {...props} />,
-  p: (props: HTMLProps<HTMLParagraphElement>) => (
-    <p className="mt-7" {...props} />
+  h3: (props) => (
+    <h3
+      className="font-semibold mt-14 mb-7 text-lapis-600 text-balance"
+      {...props}
+    />
   ),
+  ul: (props) => (
+    <ul
+      className="mt-7 list-disc list-outside marker:text-lapis-200 pl-5"
+      {...props}
+    />
+  ),
+  ol: (props) => (
+    <ol
+      className="mt-7 list-decimal list-outside marker:text-lapis-200 pl-5"
+      {...props}
+    />
+  ),
+  li: (props) => <li className="pl-1.5" {...props} />,
   a: ({ href, ...props }) => {
     return (
       <Link
-        className={cn(
-          "break-words decoration-from-font",
-          "underline underline-offset-2",
-          "decoration-lapis-400 hover:decoration-lapis-600 dark:decoration-lapis-200 dark:hover:decoration-lapis-100",
-          "focus:outline-none focus-visible:rounded-xs focus-visible:ring-2 focus-visible:ring-current focus-visible:ring-opacity-50 focus-visible:ring-offset-2"
-        )}
+        className="break-words decoration-from-font underline underline-offset-2 decoration-lapis-300 hover:decoration-lapis-600 focus:outline-none focus-visible:rounded-xs focus-visible:ring-2 focus-visible:ring-current focus-visible:ring-opacity-50 focus-visible:ring-offset-2"
         href={href}
         draggable={false}
         {...(href?.startsWith("https://")
@@ -42,14 +52,18 @@ export const components: Record<string, FC<any>> = {
       />
     );
   },
-  blockquote: (props: HTMLProps<HTMLQuoteElement>) => (
+  strong: (props) => <strong className="font-bold" {...props} />,
+  p: (props) => <p className="mt-7" {...props} />,
+  blockquote: (props) => (
     <blockquote
-      className="pl-6 -ml-6 sm:pl-10 sm:-ml-10 md:pl-14 md:-ml-14 text-lapis-400"
+      className="pl-6 -ml-6 sm:pl-10 sm:-ml-10 md:pl-14 md:-ml-14 text-lapis-400 border-l border-lapis-900"
       {...props}
     />
   ),
+  pre: (props) => (
+    <pre className="mt-7 whitespace-pre md:whitespace-pre-wrap" {...props} />
+  ),
 };
-
 export function useMDXComponents(inherited: MDXComponents): MDXComponents {
   return {
     ...inherited,
